c73337220f44d748414469827138464940d13e6c
JaehyeonSung <top91652@gmail.com>
2024-09-26 09:20:20 +0900

    register/login function added

--- app.py
+++ app.py
@@ -1,15 +1,73 @@
-from quart import Quart, request, jsonify, render_template
+from quart import Quart, request, jsonify, render_template, redirect, url_for, flash, session
 from bs4 import BeautifulSoup
-import requests, asyncio
+import requests, asyncio, re, aiohttp, asyncpg
 from transformers import BartTokenizer, BartForConditionalGeneration, MBart50Tokenizer, MBartForConditionalGeneration
-import re
-import aiohttp
+from psycopg2 import pool
+from flask_cors import CORS
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 app = Quart(__name__)
-
+# CORS(app)
 # BART 모델 및 토크나이저
 bart_tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
 bart_model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+
+# 비동기 PostgreSQL 연결 설정
+async def init_db():
+    return await asyncpg.connect(
+        user='postgres',
+        password='1234',
+        database='NewsSummarization',
+        host='localhost'
+    )
+
+# Postgre SQL 연결 Pool 설정
+# db_pool = None
+
+# # Init db_pool
+# def init_db_pool():
+#     global db_pool
+#     try:
+#         db_pool = psycopg2.pool.SimpleConnectionPool(
+#             1, 20,
+#             host ="localhost",
+#             database = "NewsSummarization",
+#             user="postgres",
+#             password="1234"
+#         )
+#         if db_pool:
+#             print("PostgreSQL connection pool created successfully.")
+#     except Exception as error:
+#         print(f"Error creating PostgreSQL connection pool: {error}")
+
+# async def get_db_connection():
+#     try:
+#         connection=db_pool.getconn()
+#         if connection:
+#             print("PostgreSQL connection from pool acuired")
+#             return connection
+#     except Exception as error:
+#         print(f"Error getting connection : {error}")
+
+# async def close_db_connection(connection):
+#     if db_pool:
+#         db_pool.putconn(connection)
+#         print("PostgreSQL connection returned to pool.")
+
+
+# @app.before_serving
+# async def startup():
+#     init_db_pool()
+
+# @app.after_serving
+# async def cleanup():
+#     if db_pool:
+#         db_pool.closeall()
+#         print("PostgreSQL connection pool closed.")
+    
+
+ 
 
 # 비동기 HTML 가져오기
 async def fetch(session, url):
@@ -63,42 +121,41 @@ async def fetch_news_by_category(session, category_url):
 # 번역 모델 및 토크나이저
 translation_tokenizer = MBart50Tokenizer.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
 translation_model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
-# 파파고를 사용한 
-def translate_text_papago(text, source_lang, target_lang, client_id, client_secret):
-    url = "https://openapi.naver.com/v1/papago/n2mt"
-    headers = {
-        "X-Naver-Client-Id": client_id,
-        "X-Naver-Client-Secret": client_secret,
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-    }
-    data = {
-        "source": source_lang,
-        "target": target_lang,
-        "text": text
-    }
-    response = requests.post(url, headers=headers, data=data)
-    response.raise_for_status()  # 응답 결과 확인용
-    result = response.json()
-    return result['message']['result']['translatedText']
-
+# def translate_text_papago(text, source_lang, target_lang, client_id, client_secret):
+    # url = "https://openapi.naver.com/v1/papago/n2mt"
+    # headers = {
+    #     "X-Naver-Client-Id": client_id,
+    #     "X-Naver-Client-Secret": client_secret,
+    #     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+    # }
+    # data = {
+    #     "source": source_lang,
+    #     "target": target_lang,
+    #     "text": text
+    # }
+    # response = requests.post(url, headers=headers, data=data)
+    # response.raise_for_status()  # 응답 결과 확인용
+    # result = response.json()
+    # return result['message']['result']['translatedText']
+# 기사 내용 정리하기 (공백, 특수문자 제거 등)
 def clean_text(text):
     text = BeautifulSoup(text, "html.parser").get_text()
     text = re.sub(r'\s+', ' ', text)
     text = re.sub(r'[^\w\s.,!?가-힣]', '', text)
     return text.strip()
-
+# 기사 내용 분리하기
 def truncate_text(text, max_length=1024):
     tokens = bart_tokenizer.encode(text, truncation=False)
     if len(tokens) > max_length:
         tokens = tokens[:max_length]
         text = bart_tokenizer.decode(tokens, skip_special_tokens=True)
     return text
-
+# 기사 내용 요약하기
 def summarize_text(text):
     inputs = bart_tokenizer(text, return_tensors="pt", truncation=True, max_length=1024)
     summary_ids = bart_model.generate(
         inputs['input_ids'],
-        max_length=300,   # bart모델 생성되는 요약의 최대 길이
+        max_length=200,   # bart모델 생성되는 요약의 최대 길이
         min_length=100,   # bart모델 생성되는 요약의 최소 길이  
         length_penalty=0.5,
         num_beams=2,
@@ -106,7 +163,7 @@ def summarize_text(text):
     )
     summary = bart_tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
-
+# 기사 내용 번역하기
 def translate_text(text, src_lang, tgt_lang):
     # Set source and target language codes
     translation_tokenizer.src_lang = src_lang
@@ -158,12 +215,12 @@ async def fetch_article(url):
     text = soup.get_text(separator='\n', strip=True)
 
     return text
-
+# 홈 화면
 @app.route('/')
 async def home():
-    return await app.send_static_file('home.html')
+    return await render_template('home.html')
 
-
+# 요약 기능 경로
 @app.route('/summarize', methods=['POST'])
 async def summarize():
     data = await request.get_json()
@@ -198,7 +255,7 @@ async def summarize():
     except Exception as e:
         print(f'Error: {str(e)}')
         return jsonify({'summary': f'Error processing the URL: {str(e)}'})
-    
+# 뉴스 크롤링 호출 경로
 @app.route('/news')
 async def get_news():
     category = request.args.get('category', '정치')
@@ -219,17 +276,140 @@ async def get_news():
         except Exception as e:
             print(f"Error fetching news: {e}")
             return jsonify({"error": "뉴스를 가져오는 데 실패했습니다."}), 500
+# 최신뉴스보기 경로
 @app.route('/watchnews')
 async def watchnews():
-    return await app.send_static_file('watchnews.html')
+    return await render_template('watchnews.html')
 
-
+# 요약 페이지 경로
 @app.route('/summarizepage', methods = ['POST', 'GET'])
 async def summarizepage():
-    return await app.send_static_file('summarizepage.html')
+    return await render_template('summarizepage.html')
 
+
+
+
+@app.route('/register', methods=['POST'])
+async def register():
+    try:
+        # 폼 데이터 가져오기
+        form_data = await request.form
+        userid = form_data.get('userid')
+        password = form_data.get('password')
+        email = form_data.get('email')
+        hashed_password = generate_password_hash(password)
+        conn = await init_db()
+
+        # 데이터 유효성 검사
+        if not userid or not password or not email:
+            return await render_template('register.html', error='모든 필드를 입력해야 합니다.')
+
+        # ID 중복 체크
+        user = await conn.fetchrow("SELECT * FROM users WHERE user_id = $1", userid)
+        if user:
+            return await render_template('register.html', error='이미 존재하는 아이디입니다.')
+
+        # 회원정보 저장
+        await conn.execute(
+            "INSERT INTO users (user_id, user_password, user_email) VALUES ($1, $2, $3)",
+            userid, hashed_password, email
+        )
+        
+        # DB 연결 닫기
+        await conn.close()
+        
+        return redirect(url_for('registerdone_page'))
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return await render_template('register.html', error='서버 오류가 발생했습니다.')
+
+# 회원가입
+@app.route('/register', methods=['GET'])
+async def register_page():
+    return await render_template('register.html')
+
+@app.route('/check-username', methods=['POST'])
+async def check_username():
+    try:
+        data = await request.get_json()
+        print(data)
+        userid = data['userid']
+
+        conn = await init_db()
+
+        # ID 중복 체크
+        user = await conn.fetchrow("SELECT * FROM users WHERE user_id = $1", userid)
+        await conn.close()
+
+        if user:
+            return jsonify({'message': '이미 존재하는 아이디입니다.'}), 400
+        else:
+            return jsonify({'message': '사용 가능한 아이디입니다.'}), 200
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'message': '서버 오류가 발생했습니다.'}), 500
+
+@app.route('/registerdone.html', methods=['GET'])
+async def registerdone_page():
+    return await render_template('registerdone.html')
+
+# 로그인 라우트
+@app.route('/login', methods=['GET', 'POST'])
+async def login():
+    if request.method == 'POST':
+        userid = (await request.form)['userid']
+        password = (await request.form)['password']
+        conn = await init_db()
+        # 데이터베이스에서 유저 정보 확인
+        user = await conn.fetchrow("SELECT * FROM users WHERE user_id = $1", userid)
+        print(password)
+        print(user['user_password'])
+        if user:
+            print(check_password_hash(user['user_password'], password))
+            if check_password_hash(user['user_password'], password):
+                print("ifentered")
+                session['userid'] = userid
+                # 관리자 아이디인 경우
+                if userid == 'admin':
+                    return redirect(url_for('admin_page'))
+                else:
+                    print(111111)
+                    flash('로그인이 완료되었습니다.', 'success')
+                    return redirect(url_for('home'))
+            else:
+                print("비밀번호가 잘못되었습니다.")
+                return redirect(url_for('login'))
+        else:
+            print(222222)
+            print('아이디가 존재하지 않습니다.')
+        
+
+            
+    print(333333)
+    return await render_template('login.html')
+
+
+# 관리자 페이지 라우트
+@app.route('/admin')
+async def admin_page():
+    return "관리자 페이지입니다."
+
+# 로그아웃 라우트
+@app.route('/logout')
+async def logout():
+    session.pop('userid', None)
+    return redirect(url_for('login'))
+
+# 세션을 위한 비밀키 설정
+app.secret_key = 'your_secret_key'
+
+
+# 앱 실행 설정
 async def run_app():
     await app.run_task(host='0.0.0.0', port=8000)
-
+# 앱 실행
 if __name__ == '__main__':
+
     asyncio.run(run_app())
